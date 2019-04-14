9c7b01528984e4a067eb38aa623b2330b6d0b792
Jaime Carlson <jaimecarlson@Jaimes-MacBook-Pro.local>
2019-04-14 16:28:04 -0500

    get first and last name to append

--- client.js
+++ client.js
@@ -9,10 +9,43 @@ function readyNow(){
 
 function displayNames(){
     console.log('The Enter Button Was CLicked');
-    $('#enterButton').append(`
+    let firstNames = $('#firstName').val();
+   console.log($('#firstName').val());
+
+    let lastNames = $('#lastName').val();
+    console.log($('#lastName').val());
+
+    
+
+
+    $('#tbodyID').append(`
+    <tr>
+        <td>
+            ${firstNames}
+        </td>
+    </tr>
+    <tr>
+        <td>
+             ${lastNames}
+         </td>
+     </tr>
+     <tr>
+        <td>
+            ${idNumber}
+        </td>
+    </tr>
+    <tr>
+        <td>
+            ${jobTitle}
+        </td>
+    </tr>
+    <tr>
+        <td>
+            ${annualSalart}
+        </td>
+    </tr>
     
     
-    
-    
-    `)
-}
+     `)//end append
+
+}//end displayNames
